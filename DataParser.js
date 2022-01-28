9a7a19d3a056a50828d9525c0a4621e86defd34d
Sznio <58606210+Sznio@users.noreply.github.com>
2022-01-28 01:24:30 +0100

    Update DataParser.js

--- DataParser.js
+++ DataParser.js
@@ -3,27 +3,41 @@ const EventEmitter = require("events");
 class DataParser extends EventEmitter {
       buf = "";
       words = new Array();
-     
-      streamWordsArray = () => {
+
+      streamWordsArray() {
             this.emit("data", this.words);
             this.words = [];
-      };
+      }
 
-      parseDataIntoWords = (data) => {
+      parseDataIntoWords(data) {
             data = this.buf + data;
-            let splitData = data.split(/ |\n|\t/g);
-            splitData = splitData.filter((n) => {
-                  return n != "\n" && n != " " && n != "\t";
-            });
+            //split by whitespace, remove whitespace elements unless they are last in the split
+            let splitData = data
+                  .split(/ +|\t+|\n+|\r+/)
+                  .map((el, index, arr) => {
+                        if (index != arr.length - 1) {
+                              return el.trim();
+                        }
+                        return el;
+                  })
+                  .filter((el, index, arr) => {
+                        if (index != arr.length - 1) {
+                              return arr.length;
+                        }
+                        return true;
+                  });
+            //wihtout the last one as it will be assigned to this.buf
             let processedData = splitData.slice(0, splitData.length - 1);
 
+            //add new words
             this.words = this.words.concat(processedData);
+
             this.streamWordsArray();
 
             this.buf = splitData[splitData.length - 1];
-      };
+      }
 
-      parseLastData = () => {
+      parseLastData() {
             if (
                   this.buf.length &&
                   this.buf != " " &&
@@ -33,7 +47,7 @@ class DataParser extends EventEmitter {
                   this.words.push(this.buf);
             }
             this.emit("end", this.words);
-      };
+      }
 
       constructor(path, options) {
             super();
